8dc4fb5ec04f1a73bc558d6c056c0dde054f16b8
Saif AlAbabseh <107197088+SaifAlAbabseh@users.noreply.github.com>
2022-07-12 14:04:56 +0300

    Delete All_Chat desktop app directory

--- All_Chat desktop app/mainClass.java
+++
@@ -1,9 +0,0 @@
-package all_chat;
-public class mainClass {
-	public static void main(String[] args) {
-		startApp();
-	}
-	private static void startApp() {
-		new loginScreen().setVisible(true);
-	}
-}
