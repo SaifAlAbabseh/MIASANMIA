bf05ef03796bacc485fbbbfc1e6e603404cb06aa
Saif AlAbabseh <107197088+SaifAlAbabseh@users.noreply.github.com>
2022-07-12 14:05:15 +0300

    Add files via upload

---
+++ All_Chat desktop app/DBInfo.java
@@ -0,0 +1,7 @@
+package all_chat;
+
+public class DBInfo {
+	public static String db = "jdbc:mysql://localhost/allchat";
+	public static String user = "root";
+	public static String pass = "12345678";
+}
